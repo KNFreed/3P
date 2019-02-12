92e015bae28c84b3f7d0ca4117812c3236cd2d8c
KNFreed <spataro.frederic@gmail.com>
2019-02-12 22:52:01 +0100

    Cleaner than ever

--- firstrun.py
+++ firstrun.py
@@ -6,13 +6,21 @@ from PIL import ImageTk
 
 window = Tk()
 window.title('3P! The Console')
-time1 = ''
-updatedhour='None'
+
 ### Window size
 # Uncomment this one for a rasp install
 #window.attributes('-fullscreen', True)
 # Uncomment this one for a windows install
 window.geometry("1200x600")
+
+###Variables
+#Clock variables
+time1 = ''
+updatedhour='None'
+#Background color of the UI
+main_background='#696969'
+#Playbutton Background
+
 
 ### Making a grid to place objects
 window.rowconfigure(0,weight=0)
@@ -23,7 +31,7 @@ window.columnconfigure(1,weight=1, uniform='third')
 window.columnconfigure(2,weight=1, uniform='third')
 
 ### Creating the infobar
-canvas = Canvas(window, height=20, bg='dark grey', bd=0, highlightthickness=0,)
+canvas = Canvas(window, height=20, bg=main_background, bd=0, highlightthickness=0,)
 canvas.grid(row=0, column=0, columnspan=3, sticky='EWNS')
 
 ### Creating the time widget
@@ -34,25 +42,16 @@ def tick():
         time1 = time2
         if updatedhour!='':
             canvas.delete(updatedhour)
-            updatedhour = canvas.create_text(30, 10, text=time1, font="Helvetica", fill="blue")
+            updatedhour = canvas.create_text(30, 10, text=time1, font="Helvetica", fill="white")
     window.after(1000, tick)
 tick()
 
-# variables for the play button background
-path = r"./games/background/"
-greypath = r"./games/background/grey/"
-random_background = random.choice([
-    x for x in os.listdir(path)
-    if os.path.isfile(os.path.join(path, x))
-])
-background_path = str(path + random_background)
-greybackground_path = str(greypath + random_background)
-background = ImageTk.PhotoImage(master=window, file=background_path)
-greybackground = ImageTk.PhotoImage(master=window, file=greybackground_path)
-
-
-def selectbackground():
-    global path, random_background, background_path, background, greypath, greybackground
+def menu():
+    playfont = font.Font(family='Helvetica', size=40, weight='bold')
+    ### Playbutton
+    #Selects random background for the PlayButtonn
+    path = r"./games/background/"
+    greypath = r"./games/background/grey/"
     random_background = random.choice([
         x for x in os.listdir(path)
         if os.path.isfile(os.path.join(path, x))
@@ -61,14 +60,6 @@ def selectbackground():
     greybackground_path = str(greypath + random_background)
     background = ImageTk.PhotoImage(master=window, file=background_path)
     greybackground = ImageTk.PhotoImage(master=window, file=greybackground_path)
-    print
-
-
-def menu():
-    playfont = font.Font(family='Helvetica', size=40, weight='bold')
-    ### Playbutton
-    # Select the button background
-    selectbackground()
     # Define the playbutton
     playbutton = Button(window, image=background, bd=0, highlightthickness=0, text="Play", font=playfont, fg="white", command=menu)
     playbutton.grid(row=1, column=0, columnspan=2, sticky='EWNS')
