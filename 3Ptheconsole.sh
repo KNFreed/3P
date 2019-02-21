a67d4334618af73640b6ab6041fe60e3e05ff02f
Frédéric Spataro <44784500+KNFreed@users.noreply.github.com>
2019-02-21 19:51:49 +0100

    Update 3Ptheconsole.sh

--- 3Ptheconsole.sh
+++ 3Ptheconsole.sh
@@ -1,1 +1,12 @@
+# Install Script for the 3P Console
 
+# Updates everything
+apt-get update && apt-get upgrade -y
+
+# Install useful packages
+apt-get install matchbox-keyboard -y
+
+# Install needed libraries
+
+sudo pip3 install pillow --update
+sudo pip3 install wifi
