371724fb47872b7c6b51fc2869be184496009fc3
KNFreed <spataro.frederic@gmail.com>
2019-02-21 23:26:23 +0100

    fixed autoupdate

--- autoupdate.py
+++ autoupdate.py
@@ -1,6 +1,7 @@
 #imports
-import git
+import git, time
 from tkinter import *
+from PIL import ImageTk
 
 window = Tk()
 window.title('3P!U The Updater')
@@ -11,6 +12,17 @@ window.title('3P!U The Updater')
 # Uncomment this one for a windows install
 window.geometry("1200x600")
 
-repo = git.Repo('./')
-update = repo.remotes.origin
-update.pull()
+backgroundImage = ImageTk.PhotoImage(file="./files/updating.png")
+background_label = Label(window, image=backgroundImage)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+def update():
+    repo = git.Repo('./')
+    update = repo.remotes.origin
+    update.pull()
+    exec(open('Ui.py').read())
+    time.sleep(2)
+    window.destroy
+time.sleep(2)
+update()
+mainloop()
