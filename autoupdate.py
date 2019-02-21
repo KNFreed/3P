b40426519dbfae89b28bde1148f639d6c9c46915
KNFreed <spataro.frederic@gmail.com>
2019-02-22 00:11:05 +0100

    One more update
    There's never enough updates

--- autoupdate.py
+++ autoupdate.py
@@ -1,28 +1,11 @@
 #imports
-import git, time
-from tkinter import *
-from PIL import ImageTk
-
-window = Tk()
-window.title('3P!U The Updater')
-
-### Window size
-# Uncomment this one for a rasp install
-#window.attributes('-fullscreen', True)
-# Uncomment this one for a windows install
-window.geometry("1200x600")
-
-backgroundImage = ImageTk.PhotoImage(file="./files/updating.png")
-background_label = Label(window, image=backgroundImage)
-background_label.place(x=0, y=0, relwidth=1, relheight=1)
+import git, time, os
 
 def update():
     repo = git.Repo('./')
     update = repo.remotes.origin
     update.pull()
-    exec(open('Ui.py').read())
+    os.system('python UI.py')
     time.sleep(2)
     window.destroy
-time.sleep(2)
 update()
-mainloop()
