d576bce8e66136830d4bc991d3d2061297ebb4d0
Frédéric Spataro <44784500+KNFreed@users.noreply.github.com>
2019-03-15 22:10:22 +0100

    Update 3Ptheconsole.sh

--- 3Ptheconsole.sh
+++ 3Ptheconsole.sh
@@ -3,9 +3,7 @@
 # Updates everything
 sudo apt-get update --yes --force-yes
 sudo apt-get upgrade --yes --force-yes
-sudo apt install --yes --force-yes \
-    git \
-    matchbox-keyboard \
+sudo apt install --yes --force-yes matchbox-keyboard
 # Install needed libraries
 sudo pip3 install pillow
 sudo pip3 install wifi
