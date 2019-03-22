b8d33888c6a8153369d02e1b2484794778120062
Frédéric Spataro <spataro.frederic@gmail.com>
2019-03-22 09:16:50 +0100

    bash update

--- 3Ptheconsole.sh
+++ 3Ptheconsole.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Install Script for the 3P Console
 # Updates everything
-sudo apt-get update --yes --force-yes
-sudo apt-get upgrade --yes --force-yes
-sudo apt install --yes --force-yes matchbox-keyboard
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt install -y matchbox-keyboard
 # Install needed libraries
 sudo pip3 install pillow
 sudo pip3 install wifi
 sudo pip3 install GitPython
-sudo apt install python3-tk
+sudo apt install -y python3-tk
