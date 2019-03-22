421fafc55beaa4eb4aa695693cda5ac9f2d36e23
Frédéric Spataro <spataro.frederic@gmail.com>
2019-03-22 16:53:28 +0100

    Dark/Light mode Switcher

--- UI.py
+++ UI.py
@@ -1,7 +1,7 @@
 #imports
 from tkinter import *
 from tkinter import font
-import random, os, time, sys, feedparser
+import random, os, time, sys
 from PIL import ImageTk
 
 
@@ -10,9 +10,9 @@ window.title('3P! The Console')
 
 ### Window size
 # Uncomment this one for a rasp install
-window.attributes('-fullscreen', True)
+#window.attributes('-fullscreen', True)
 # Uncomment this one for a windows install
-#window.geometry("1200x600")
+window.geometry("1200x600")
 
 ###Variables
 #Clock variables
@@ -21,22 +21,24 @@ updatedhour='None'
 
 
 ### Making a grid to place objects
-window.rowconfigure(0,weight=0)
-window.rowconfigure(1,weight=1, uniform='semi')
-window.rowconfigure(2,weight=1, uniform='semi')
-window.columnconfigure(0,weight=1, uniform='third')
-window.columnconfigure(1,weight=1, uniform='third')
-window.columnconfigure(2,weight=1, uniform='third')
-
+def configure():
+    window.rowconfigure(0,weight=0)
+    window.rowconfigure(1,weight=1, uniform='semi')
+    window.rowconfigure(2,weight=1, uniform='semi')
+    window.columnconfigure(0,weight=1, uniform='third')
+    window.columnconfigure(1,weight=1, uniform='third')
+    window.columnconfigure(2,weight=1, uniform='third')
+configure()
 
 ### Creating variables for the background color
     # Will be modified with a local variable later
 backgroundcolor='#696969'
 infobarbackground="#4f4f4f"
+textcolor="white"
 window.configure(background=backgroundcolor)
 
 ### Creating the infobar
-canvas = Canvas(window, height=20, bg=infobarbackground, bd=0, highlightthickness=0,)
+canvas = Canvas(window, height=20, bg=infobarbackground, bd=0, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=3, sticky='EWNS')
 
 ### Creating the time widget
@@ -56,7 +58,8 @@ def stop():
 
 ### UI
 def menu():
-
+    configure()
+    canvas.grid(row=0, column=0, columnspan=6, sticky='EWNS')
     # Useful defs
     def destroymenu():
         playbutton.destroy()
@@ -78,6 +81,7 @@ def menu():
     greybackground_path = str(greypath + random_background)
     background = ImageTk.PhotoImage(master=window, file=background_path)
     greybackground = ImageTk.PhotoImage(master=window, file=greybackground_path)
+    arrow = ImageTk.PhotoImage(master=window, file="./files/return_arrow2.png")
     # Define the playbutton
     playbutton = Button(window, image=background, bd=0, highlightthickness=0, text="Play", font=playfont, fg="white", command=menu)
     playbutton.grid(row=1, column=0, columnspan=2, sticky='EWNS')
@@ -126,6 +130,71 @@ def menu():
     def settings():
         # Destroy all the buttons that existed
         destroymenu()
+
+        #configure the grid
+        window.rowconfigure(0, weight=0)
+        window.rowconfigure(1, weight=1, uniform='fifth')
+        window.rowconfigure(2, weight=1, uniform='fifth')
+        window.rowconfigure(3, weight=1, uniform='fifth')
+        window.rowconfigure(4, weight=1, uniform='fifth')
+        window.rowconfigure(5, weight=1, uniform='fifth')
+        window.rowconfigure(6, weight=1, uniform='fifth')
+        window.columnconfigure(0, weight=1)
+        window.columnconfigure(1, weight=1, uniform='sixth')
+        window.columnconfigure(2, weight=1, uniform='sixth')
+        canvas.grid(row=0, column=0, columnspan=3, sticky='EWNS')
+
+        # Title Canvas
+        title_canvas = Canvas(window, bg="#ffbe4d", bd=0, highlightthickness=0)
+        title_canvas.create_text(400, 47.5, font=(playfont, 35), text="Settings", fill="white")
+        title_canvas.grid(row=1, column=1, columnspan=2, sticky='EWNS')
+
+        #Return button
+        def returnmenu():
+            title_canvas.destroy()
+            returnbutton.destroy()
+            menu()
+
+        def switchmode():
+            def exitswitchmode():
+                newindow.destroy()
+            newindow = Canvas(window, bg=backgroundcolor, bd=0, highlightthickness=0)
+            newindow.grid(row=1, column=0, columnspan=3, rowspan=7, sticky="EWNS")
+
+            def darkmodechoice():
+                global backgroundcolor, textcolor
+                backgroundcolor = '#696969'
+                textcolor = "white"
+                window.configure(background=backgroundcolor)
+                exitswitchmode()
+                returnmenu()
+
+            def lightmodechoice():
+                global backgroundcolor, textcolor
+                backgroundcolor = 'white'
+                textcolor = "black"
+                window.configure(background=backgroundcolor)
+                exitswitchmode()
+                returnmenu()
+
+            returntosettings = Button(window, bd=0, highlightthickness=0, text="Return", font=playfont, fg=textcolor,
+                              command=exitswitchmode, bg=backgroundcolor, anchor="center")
+            returntosettings_window = newindow.create_window(600, 580, anchor=S, window=returntosettings)
+
+            darkmode = Button(window, bd=0, highlightthickness=0, text="Dark Mode", font=playfont, fg=textcolor,
+                                      command=darkmodechoice, bg=backgroundcolor, anchor="center")
+            darkmode_window = newindow.create_window(200, 300, anchor=S, window=darkmode)
+
+            lightmode = Button(window, bd=0, highlightthickness=0, text="Light Mode", font=playfont, fg=textcolor,
+                              command=lightmodechoice, bg=backgroundcolor, anchor="center")
+            lightmode_window = newindow.create_window(1000, 300, anchor=S, window=lightmode)
+
+        returnbutton = Button(window, bd=0, highlightthickness=0, text="Return", font=playfont, fg=textcolor, command=returnmenu, bg="orange")
+        returnbutton.grid(row=1, column=0, columnspan=1, sticky='EWNS')
+
+        colormode_settings = Button(window, bd=2, highlightthickness=0, text="Switch Dark/Light mode", font=playfont, fg=textcolor,
+                              command=switchmode, bg=backgroundcolor, anchor="w")
+        colormode_settings.grid(row=2, column=0, columnspan=3, sticky='EWNS')
 
     settingsbutton = Button(window, bg="#ffbe4d", bd=0, highlightthickness=0, text="Settings", font=playfont, fg="white", command=settings)
     settingsbutton.grid(row=2, column=1, columnspan=1, sticky='EWNS')
